6950a2436276ab2a6a175cb2d5ecf22b94319b57
donLionski <wisskirchen@womix.de>
2017-06-02 16:43:46 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="de">
+
+<head>
+    <meta charset="UTF-8">
+    <title>
+</title>
+    <link rel="stylesheet" href="css/main.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
+    
+    <meta name="robots" content="noindex, nofollow">
+
+
+</head>
+<body>
+
+
+
+
+
+        <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+        
+<!--     Skripte für Gsap und Scrollmagic
+
+        <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.0/TweenMax.min.js"></script>
+
+        <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/ScrollMagic.js"></script>
+        <script src="//cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/debug.addIndicators.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/easing/EasePack.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/TimelineMax.min.js"></script>
+
+                <script src="js/jquery.mousewheel.js" type="text/javascript"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/ScrollMagic/2.0.5/plugins/animation.gsap.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/gsap/1.19.1/plugins/ScrollToPlugin.min.js"></script>
+-->
+
+        <script src="js/main.js" type="text/javascript"></script>
+        
+
+</body>
+
+</html>
